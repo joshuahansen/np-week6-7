c15cef40f3b78ede778e9c65bfd4ff26d30b1a7f
joshuahansen <joshua.hansen188@gmail.com>
2018-04-10 09:24:59 +1000

    task 1 complete. using customRunnable class to set thread

---
+++ task1.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.util.zip.*;
+
+/**
+*Task 1 
+*Recieve input from console
+*Pass input to thread
+*Print out result
+**/
+class Task1
+{
+    public static void main(String args[])
+    {
+        try {
+            System.out.println("Enter input:");
+            BufferedReader keyboardInput = new BufferedReader(new InputStreamReader(System.in));
+        
+            String userInput;
+            while((userInput = keyboardInput.readLine()) != null)
+            {
+                //create thread from CustomRunnable class
+                Thread firstThread = new Thread(new CustomRunnable(userInput));
+                //start thread
+                firstThread.start();
+                break;
+            }
+            //close keyboard input
+            keyboardInput.close();
+        }catch(IOException ex)
+        {
+            System.out.println("Error with Input/Output: " + ex);
+        }
+    }
+}
+
+/**
+*Custom runnable class
+*prints out user input set in constructor
+**/
+class CustomRunnable implements Runnable
+{
+    private String input;
+    //constructor for runnable. sets user input.
+    public CustomRunnable(String input)
+    {
+        this.input = input;
+    }
+    //override runnable run fuction to print out user input
+    @Override
+    public void run()
+    {
+        System.out.println("Thread: " + this.input);
+    }
+}
