fb7b844ad13e257a580a78795ab6aa6de910ed50
Josh <joshua.hansen188@gmail.com>
2018-04-15 11:47:36 +1000

    task 2 complete

--- task3.java
+++ task3.java
@@ -4,14 +4,15 @@ import java.lang.*;
 import java.util.zip.*;
 
 /**
-*Task 2
-*Data sharing between 2 threads
+*Task 3
+*2 threads run in a loop of input-then-print
 *Recieve input from console
 *Pass input to thread
 *Print out result
+*Exit on single X character
 **/
 
-class Task2
+class Task3
 {
     public static void main(String args[])
     {
@@ -23,24 +24,27 @@ class Task2
             userInput = keyboardInput.readLine();
             
             //create thread from CustomRunnable class
-            Thread inputThread = new InputThread(userInput);
+            Thread inputThread = new InputThread();
             Thread printerThread = new PrinterThread();
-            
-            //start thread
-            inputThread.start();
-            
-            /*
-            * have printer wait for inputThread to be completed
-            * before running.
-            */
-            try {
-                inputThread.join();
-            }catch(InterruptedException e)
+         
+            while(!userInput.equals("X"))
             {
-                e.printStackTrace();
+                //start thread
+                inputThread.start();
+                inputThread.setInput(userInput);
+                /*
+                * have printer wait for inputThread to be completed
+                * before running.
+                */
+                try {
+                    inputThread.join();
+                }catch(InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                printerThread.start();
+                userInput = keyboardInput.readLine();
             }
-            printerThread.start();
-            
             //close keyboard input
             keyboardInput.close();
         }catch(IOException ex)
@@ -57,16 +61,15 @@ class Task2
 class InputThread extends Thread
 {
     private String input;
-    //constructor for thread sets keyboard input to global.
-    public InputThread(String input)
-    {
-        this.input = input;
-    }
     //override runnable run fuction to print out user input
     @Override
     public void run()
     {
         Global.string = this.input;
+    }
+    public void setInput(String input)
+    {
+        this.input = input;
     }
 }
 
